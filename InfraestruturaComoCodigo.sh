4c85dfe181c0354000ac6a8734b74173522be33f
Mariana Selene <mariana02301@gmail.com>
2025-04-04 17:46:28 -0300

    Update InfraestruturaComoCodigo.sh

--- InfraestruturaComoCodigo.sh
+++ InfraestruturaComoCodigo.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-sudo mkdir /publico 
-sudo mkdir /adm 
-sudo mkdir /ven 
-sudo mkdir /sec 
+mkdir /publico 
+mkdir /adm 
+mkdir /ven 
+mkdir /sec 
 
-sudo groupadd GRP_ADM 
-sudo groupadd GRP_VEN 
-sudo groupadd GRP_SEC 
+groupadd GRP_ADM 
+groupadd GRP_VEN 
+groupadd GRP_SEC 
 
-sudo adduser Carlos -m -s /bin/bash -p $(openssl passwd -6 123) -G GRP_ADM 
-sudo adduser Maria -m -s /bin/bash -p $(openssl passwd -6 123) -G GRP_ADM
-sudo adduser Joao -m -s /bin/bash -p $(openssl passwd -6 123) -G GRP_ADM
+adduser Carlos -m -s /bin/bash -p $(openssl passwd -6 123) -G GRP_ADM 
+adduser Maria -m -s /bin/bash -p $(openssl passwd -6 123) -G GRP_ADM
+adduser Joao -m -s /bin/bash -p $(openssl passwd -6 123) -G GRP_ADM
 
-sudo adduser Debora -m -s /bin/bash -p $(openssl passwd -6 123) -G GRP_VEN
-sudo adduser Sebastiana -m -s /bin/bash -p $(openssl passwd -6 123) -G GRP_VEN
-sudo adduser Roberta -m -s /bin/bash -p $(openssl passwd -6 123) -G GRP_VEN
+adduser Debora -m -s /bin/bash -p $(openssl passwd -6 123) -G GRP_VEN
+adduser Sebastiana -m -s /bin/bash -p $(openssl passwd -6 123) -G GRP_VEN
+adduser Roberta -m -s /bin/bash -p $(openssl passwd -6 123) -G GRP_VEN
  
-sudo adduser Josefina -m -s /bin/bash -p $(openssl passwd -6 123) -G GRP_SEC
-sudo adduser Amanda -m -s /bin/bash -p $(openssl passwd -6 123) -G GRP_SEC
-sudo adduser Rogerio -m -s /bin/bash -p $(openssl passwd -6 123) -G GRP_SEC
+adduser Josefina -m -s /bin/bash -p $(openssl passwd -6 123) -G GRP_SEC
+adduser Amanda -m -s /bin/bash -p $(openssl passwd -6 123) -G GRP_SEC
+adduser Rogerio -m -s /bin/bash -p $(openssl passwd -6 123) -G GRP_SEC
 
 chown root:GRP_ADM /adm
 chown root:GRP_VEN /ven
