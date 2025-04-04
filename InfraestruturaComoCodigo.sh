5ab6a9d022c6044f688400865d49a3f3cb829398
Mariana Selene <mariana02301@gmail.com>
2025-04-04 17:45:25 -0300

    Update InfraestruturaComoCodigo.sh

--- InfraestruturaComoCodigo.sh
+++ InfraestruturaComoCodigo.sh
@@ -10,16 +10,16 @@ sudo groupadd GRP_VEN
 sudo groupadd GRP_SEC 
 
 sudo adduser Carlos -m -s /bin/bash -p $(openssl passwd -6 123) -G GRP_ADM 
-sudo adduser Maria -m -s /bin/bash -p $(openssl passwd -6 123)-G GRP_ADM
+sudo adduser Maria -m -s /bin/bash -p $(openssl passwd -6 123) -G GRP_ADM
 sudo adduser Joao -m -s /bin/bash -p $(openssl passwd -6 123) -G GRP_ADM
 
 sudo adduser Debora -m -s /bin/bash -p $(openssl passwd -6 123) -G GRP_VEN
 sudo adduser Sebastiana -m -s /bin/bash -p $(openssl passwd -6 123) -G GRP_VEN
-sudo adduser Roberrta -m -s /bin/bash -p $(openssl passwd -6 123) -G GRP_VEN
+sudo adduser Roberta -m -s /bin/bash -p $(openssl passwd -6 123) -G GRP_VEN
  
 sudo adduser Josefina -m -s /bin/bash -p $(openssl passwd -6 123) -G GRP_SEC
 sudo adduser Amanda -m -s /bin/bash -p $(openssl passwd -6 123) -G GRP_SEC
-sudo adduser Regerio -m -s /bin/bash -p $(openssl passwd -6 123) -G GRP_SEC
+sudo adduser Rogerio -m -s /bin/bash -p $(openssl passwd -6 123) -G GRP_SEC
 
 chown root:GRP_ADM /adm
 chown root:GRP_VEN /ven
@@ -28,4 +28,4 @@ chown root:GRP_SEC /sec
 chmod 777 /publico
 chmod 770 /adm
 chmod 770 /ven
-chmod 770 /sen
+chmod 770 /sec
